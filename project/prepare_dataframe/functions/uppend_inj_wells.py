6342561aeeee5ffcc20eaa5b66675c7fa267571c
Alexander <alexander.talmenev@gmail.com>
2023-04-27 09:08:16 +0300

    uppend_new_names

--- project/prepare_dataframe/functions/uppend_inj_wells.py
+++ project/prepare_dataframe/functions/uppend_inj_wells.py
@@ -1,14 +1,13 @@
 # coding: utf-8
 import pandas as pd
 
-def uppend_inj_wells(preproc_df, train_df, graph_wells):
+def uppend_inj_wells(preproc_df, train_df, distance=False, graph_wells=None):
     '''
     Добавляет закачку нагнетательных скважин, деленную на расстояние до скважины.
     preproc_df - dataframe куда добавляются данные о нагнетательных скважинах
     train_df - dataframe откуда берутся данные о нагнетательных скважинах.
+    distance - нужно ли учитывать расстояние до нагнетательных скважин.
     graph_wells - dataframe откуда берутся расстояния между скважинами.
-
-    
     '''
     if 'MEASURED_IN_DATE' not in preproc_df.columns:
         preproc_df.reset_index(inplace=True)
@@ -31,7 +30,7 @@ def uppend_inj_wells(preproc_df, train_df, graph_wells):
                                                    how='left')
     train_inj_all_date.fillna(value=0, inplace=True)
 
-
+    
 
     intake_in_date = pd.pivot_table(data=train_inj_all_date, values='INTAKE', index='MEASURED_IN_DATE', columns='WELL_NAME')
     intake_in_date.reset_index(inplace=True)
@@ -43,7 +42,9 @@ def uppend_inj_wells(preproc_df, train_df, graph_wells):
     data_with_inj_wells.set_index('MEASURED_IN_DATE', inplace=True)
     data_with_inj_wells = pd.concat([data_with_inj_wells.iloc[:, - inj_wells.shape[0] :], 
                                     data_with_inj_wells.iloc[:, : - inj_wells.shape[0]]], axis=1)
-
+    
+    if not distance:
+        return data_with_inj_wells
     
     df_inj_well_with_gaps = pd.DataFrame()
 
@@ -51,8 +52,9 @@ def uppend_inj_wells(preproc_df, train_df, graph_wells):
         data_well_oil = data_with_inj_wells[data_with_inj_wells['WELL_NAME'] == well_oil]
         data_well_oil.iloc[:, : inj_wells.shape[0]] = (data_well_oil.iloc[:, : inj_wells.shape[0]]  / 
                                                        graph_wells[inj_wells].iloc[well_oil, :])
-
+        
+        
         df_inj_well_with_gaps = pd.concat([df_inj_well_with_gaps, data_well_oil])
-
+    df_inj_well_with_gaps.fillna(value=0, inplace=True) 
 
     return df_inj_well_with_gaps
